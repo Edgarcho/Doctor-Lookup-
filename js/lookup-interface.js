6e9c7bec9139d8167bb692b5ba7def08c2037b21
Edgar Hernandez <edgarchortiz@gmail.com>
2018-02-26 15:56:13 -0800

    Add radio button to index.html

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,30 +1,35 @@
+var LookupModule = require('./lookup.js').lookupModule;
+
 $(document).ready(function(){
+  var lookupModule = new LookupModule()
   $('#searchForm').submit(function(event) {
     event.preventDefault();
     let searchInput = $('#userInput').val();
-    let location = [45.523, -122.676, 100];
+    lookupModule.getData(userInput);
+
+
     let user_key =('58be7bb0af953f79bc4096870af09a9b');
     $('#userInput').val("");
-    $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=`+ searchInput +`&location=`+ location[0] + ',' + location[1] + ',' + location[2] +`&skip=0&limit=10&user_key=`+ user_key,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        console.log(response);
-        debugger;
-        for (let i = 0; i < response.data.length; i++){
-          $('#result').append(`<ul><li>First Name: ${response.data[i].profile.first_name}</li></ul>`);
-          $('#result').append(`<ul><li>Last Name: ${response.data[i].profile.last_name}</li></ul>`);
-          $('#result').append(`<ul><li>Address: ${response.data[i].practices[i].visit_address.street}, ${response.data[i].practices[i].visit_address.city}, ${response.data[i].practices[i].visit_address.state}, ${response.data[i].practices[i].visit_address.zip}</li></ul>`);
-          $('#result').append(`<ul><li>Phone Number: ${response.data[i].practices[i].phones[i].number}</li></ul>`);
-          $('#result').append(`<ul><li>Accepts new patients: ${response.data[i].practices[i].accepts_new_patients}</li></ul><br>`);
-        }
-      },
-      error: function(){
-        $('#errors').text("There was an error processing your request. Please try again.");
-      }
-    });
+
+  });
+});
+
+/*
+var ApplicationModule = require('./scripts.js').applicationModule;
+
+var displayData = function(results) {
+  results.forEach(function(result) {
+    $('#some-unordered-list').append('<li>' + result.name + '</li>');
+  });
+}
+
+$(document).ready(function() {
+  var applicationModule = new ApplicationModule();
+
+  $('#some-form').submit(function(event) {
+    event.preventDefault();
+
+    var userInput = $('#text-input').val();
+    applicationModule.getData(userInput, displayData);
   });
 });
