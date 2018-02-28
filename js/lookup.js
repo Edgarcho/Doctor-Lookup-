ce5cdd760bd33dfd17420db85ad40688a4c1380d
Edgar Hernandez <edgarchortiz@gmail.com>
2018-02-27 17:45:24 -0800

    Refactor lookup.js with if statement

--- js/lookup.js
+++ js/lookup.js
@@ -1,19 +1,37 @@
 const apiKey = require('./../.env').apiKey;
 
 export let searchApi = function(searchInput, operator) {
-  return new Promise(function(resolve, reject){
-    let request = new XMLHttpRequest();
-    let location = [45.523, -122.676, 100];
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${searchInput}&location=${location[0]},${location[1]},${location[2]}&skip=0&limit=10&user_key=${apiKey}`;
-    console.log(url);
-    request.onload = function(){
-      if(this.status === 200){
-        resolve(request.response);
-      } else{
-        reject(Error(request.statusText));
-      }
-    };
-    request.open("GET", url, true);
-    request.send();
-  });
-};
+  if(operator === "name"){
+      return new Promise(function(resolve, reject){
+        let request = new XMLHttpRequest();
+        let location = [45.523, -122.676, 100];
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${searchInput}&location=${location[0]},${location[1]},${location[2]}&skip=0&limit=10&user_key=${apiKey}`;
+        console.log(url);
+        request.onload = function(){
+          if(this.status === 200){
+            resolve(request.response);
+          } else{
+            reject(Error(request.statusText));
+          }
+        };
+        request.open("GET", url, true);
+        request.send();
+      });
+    }else {
+      return new Promise(function(resolve, reject){
+        let request = new XMLHttpRequest();
+        let location = [45.523, -122.676, 100];
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${searchInput}&location=${location[0]},${location[1]},${location[2]}&skip=0&limit=10&user_key=${apiKey}`;
+        console.log(url);
+        request.onload = function(){
+          if(this.status === 200){
+            resolve(request.response);
+          } else{
+            reject(Error(request.statusText));
+          }
+        };
+        request.open("GET", url, true);
+        request.send();
+      });
+    }
+  };
